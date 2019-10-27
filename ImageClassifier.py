d671747396ca0a69e3b6fd05ca409c0126d2bcef
Charlotte <Charlotte.Maschke@gmx.de>
2019-10-27 16:46:14 -0400

    import display image

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -1,3 +1,13 @@
 import pandas as pd 
-train_images = pd.read_pickle('train_max_x') 
+import numpy as np
+import matplotlib.pyplot as plt
+
+train_images = pd.read_pickle('train_max_x')
 test_images = pd.read_pickle('test_max_x')
+
+
+from PIL import Image
+
+
+plt.imshow(np.array(train_images[0]), cmap='gray', vmin=0, vmax=255)
+plt.show()
