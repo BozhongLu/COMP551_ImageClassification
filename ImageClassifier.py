1a2ebc3d41531b5f36e317e796c768406937c529
hao <lihaocaleo@gmail.com>
2019-10-27 16:53:11 -0400

    upload title which shows the label for image plot

--- ImageClassifier.py
+++ ImageClassifier.py
@@ -4,10 +4,8 @@ import matplotlib.pyplot as plt
 
 train_images = pd.read_pickle('train_max_x')
 test_images = pd.read_pickle('test_max_x')
+train_labels = pd.read_csv('train_max_y.csv')
 
-
-from PIL import Image
-
-
+plt.title('Label: {}'.format(train_labels.iloc[train_images[0]]['Label']))
 plt.imshow(np.array(train_images[0]), cmap='gray', vmin=0, vmax=255)
 plt.show()
