61268d99e061899c6e52ef8280257ad0ec353f34
Charlotte <Charlotte.Maschke@gmx.de>
2019-10-31 00:04:16 -0400

    import display image

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # COMP551_ImageClassification
 We use .gitignore to ignore all the huge data files.
 Please makesure all the 3 files test_max_x train_max_x train_may_y.csv are all locally stored in your pycharm folder (COMP551_ImageCLassification)
+
+Hi, there is a new file clean_Data. After you run it you should get a new dataset without backgrounds. 
+I would like to ask you to still build your models with the normal Dataset. We will try the cleaned one afterwards. 
+Sometimes it does weird things, but in this case I really think it helps. 
---
+++ mnist_model.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+"""Variables to determine"""
+batch_size = 64
+device = torch.device("cpu")
+
+
+kwargs={}
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ])),
+    batch_size=batch_size, shuffle=True, **kwargs)
+
+test_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('data', train=False, transform=transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])),
+    batch_size=batch_size, shuffle=True, **kwargs)
+
+class TheNet(nn.Module):
+    def __init__(self):
+        super(TheNet,self).__init__()
+        # convolution layer for image processing
+        # put 4 pixels together to kernels
+        # take 1 image in anfd 10 out
+        # kernel size 5: 5x5 pixel
+        self.conv1 = nn.Conv2d(1,20,kernel_size=4)
+        self.conv2 = nn.Conv2d(20,20,kernel_size=4)
+        # useless pixels drop out
+        self.conv_dropout = nn.Dropout2d()
+        # Fullyconnected layer= normal linear layers
+        self.fc1 = nn.Linear(320,60)
+        self.fc2 = nn.Linear(60, 10)
+
+    def forward(self,x):
+        # Pictures into layer1
+        x = F.relu(self.conv1(x))
+        x = F.max_pool2d(x, 2)
+        x = F.relu(self.conv2(x))
+        x = self.conv_dropout(x)
+        x = F.max_pool2d(x, 2)
+        x = x.view(-1,320)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        # classifier, so all need to be 0 or 1
+        return F.log_softmax(x, dim=1)
+
+
+model = TheNet().to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.8)
+
+def train(epoch):
+    model.train()
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = F.nll_loss(output, target)
+        loss.backward()
+        optimizer.step()
+        if batch_idx % 10 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(train_loader.dataset),
+                       100. * batch_idx / len(train_loader), loss.item()))
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            out = model(data)
+            test_loss += F.nll_loss(out, target, size_average=False).item() # sum up batch loss
+            pred = out.data.max(1,keepdim=True)[1]
+            #pred = out.argmax(dim=1, keepdim=True) # get the index of the max log-probability
+            correct += pred.eq(target.view_as(pred)).sum()
+
+    test_loss /= len(test_loader.dataset)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+
+
+for epoch in range(1,5):
+    train(epoch)
+    test ()
+
+
+
+
+
+
+
+
+
